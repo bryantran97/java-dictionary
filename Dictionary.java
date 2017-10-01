7d73aea385bba1a5de8412e7c3416edf7b87e9f0
bryantran97@gmail.com <bryantran97@gmail.com>
2017-09-30 20:02:57 -0500

    well, it's a start!

---
+++ Dictionary.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Dictionary {
+
+	public static void main(String[] args) {
+		// Get the user input for dictionary word
+		System.out.print("Hey you... put in a dictionary word: ");
+		Scanner sc = new Scanner(System.in);
+		String word = sc.nextLine();
+		
+		System.out.println(word);
+		
+		
+	}
+
+}
