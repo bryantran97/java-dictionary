c774dd6b1f0fa05144214bc08e92624f45dfd84c
Bryan Tran <bryantran97@gmail.com>
2017-10-03 14:40:55 -0500

    update dictionary (go back and fix when @ home)

--- Dictionary.java
+++ Dictionary.java
@@ -7,60 +7,72 @@ import java.io.IOException;
 public class Dictionary {
 
 	private static final String FILENAME = "dictionary.txt";
-	
+	// ========================
+	//      KEY VALUE PAIR
+	// ========================
+	public static class keyValue {
+		int key = 0;
+		String word = null;
+		String value = null;
+		String classification = null;
+		
+		public keyValue(int keyVal, String wordVal, String classVal, String defVal) {
+			key = keyVal;
+			word = wordVal;
+			classification = classVal;
+			value = defVal;
+		}
+	}
 	// ========================
 	//      Linear Probing
 	// ========================
 	public static class LinearProbing {
-		private int currentSize, maxSize;
-		private String[] keys;
-		private String[] vals;
+		int tableSize = 13;
+		int numElements = 0;
+		keyValue[] lpArray = new keyValue[tableSize];
 		
-		// Constructor
-		public LinearProbing(int capacity){
-			currentSize = 0;
-			maxSize = capacity;
-			keys = new String[maxSize];
-			vals = new String[maxSize];
+		public int hash(String word) {
+			int hash = 0;
+			for (int i = 0; i < word.length(); i++){
+				hash = (hash << 5) - hash + word.charAt(i);
+			}
+			
+			return Math.abs(hash % 524287);
 		}
 		
-	    // Get the hash of a given key
-	    private int hash(String key) 
-	    {
-	        return key.hashCode() % maxSize;
-	    }
-	    
-	    public void insert(String key, String val){
-	    	int tmp = hash(key);
-	    	int i = tmp;
-	    	do {
-	    		if (keys[i] == null){
-	    			keys[i] = key;
-	    			vals[i] = val;
-	    			currentSize++;
-	    			return;
-	    		}
-	    		if (keys[i].equals(key)) {
-	    			vals[i] = val;
-	    			return;
-	    		}
-	    		i = (i + 1) % maxSize;
-	    	} while (i != tmp);
-	    }
-	    
-	    public String find(String key){
-	    	int i = hash(key);
-	    	while(keys[i] != null){
-	    		if(keys[i].equals(key)){
-	    			return vals[i];
-	    		}
-	    		i = (i + 1) % maxSize;
-	    	}
-	    	
-	    	return null;
-	    }
+		public int resize(int size){
+			size = 2 * size;
+			return size;
+		}
+		
+		// Insert
+		public void insert(String word, String classification, String definition){
+			int key = hash(word);	// retrieve hash key
+			keyValue kvp = new keyValue(key, word, classification, definition); // create the object
+			if(numElements >= tableSize/2) { // resize if more elements than half elements in the array
+				int newSize = resize(tableSize);
+				keyValue[] newArray = new keyValue[newSize];
+				for(int i = 0; i < tableSize; i++){
+					newArray[i] = lpArray[i];
+				}
+				// make new array
+			}
+			
+			if(lpArray[key] == null){
+				lpArray[key] = kvp;
+			} else {
+				int count = 0;
+				while(lpArray[key] != null) {
+					key = key + count++;
+				}
+				
+				lpArray[key] = kvp;
+			}
+			
+			numElements++;
+		}
 	}
-	
+			
 	// ========================
 	//      MAIN FUNCTION
 	// ========================
@@ -71,11 +83,10 @@ public class Dictionary {
 		String word = sc.nextLine();
 		// Convert the word to match the dictionary.txt
 		String converted_word = word.replaceAll(" ", "_").toLowerCase();;
-		
-		System.out.println(word + " " + converted_word);
 		sc.close();
 		
-		LinearProbing lp = new LinearProbing(500);
+		// Linear probing Array size of 524287 (prime)
+		LinearProbing lp = new LinearProbing();
 		
 		// Import dictionary
 		try (BufferedReader br = new BufferedReader(new FileReader(FILENAME))){
@@ -84,7 +95,7 @@ public class Dictionary {
 			for(int i = 0; i < 10; i++) {
 				if((sCurrentLine = br.readLine()) != null){
 					String[] parts = sCurrentLine.split(Pattern.quote("|"));
-					System.out.println(parts[0] + parts[1] + parts[2]);
+					System.out.println(lp.hash(parts[0]));
 				}
 			}
 		} catch (IOException e){
