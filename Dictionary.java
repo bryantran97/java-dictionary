494d44160f8b31a41f0d4e95c08b1a34f2c4833a
Bryan Tran <bryantran97@gmail.com>
2017-10-06 14:38:33 -0500

    added fixed size back in
    doubling array caused issues

--- Dictionary.java
+++ Dictionary.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 import java.io.BufferedReader;
@@ -27,7 +28,7 @@ public class Dictionary {
 	//      Linear Probing
 	// ========================
 	public static class LinearProbing {
-		int tableSize = 13;
+		int tableSize = 330767;
 		int numElements = 0;
 		int bounce = 0;
 		keyValue[] lpArray = new keyValue[tableSize];
@@ -46,17 +47,6 @@ public class Dictionary {
 			return tableSize;
 		}
 		
-		// resize
-		public void resize() {
-			keyValue[] doubledLPArray = new keyValue[tableSize*2];
-			for(int i = 0; i < tableSize; i++){
-				doubledLPArray[i] = lpArray[i];
-			}
-			
-			tableSize = tableSize * 2;
-			lpArray = doubledLPArray;
-		}
-		
 		// return lambda
 		public double lambda(){
 			double lambda = ((double)numElements) / tableSize;
@@ -67,10 +57,6 @@ public class Dictionary {
 		public void insert(String word, String classification, String definition){
 			int key = hash(word);	// retrieve hash key
 			keyValue kvp = new keyValue(key, word, classification, definition); // create the object
-			
-			if(numElements >= (tableSize/2)){
-				resize();
-			}
 			
 			if(lpArray[key] == null){
 				lpArray[key] = kvp;
@@ -102,7 +88,7 @@ public class Dictionary {
 	//      Quadratic Probing
 	// ========================
 	public static class QuadraticProbing {
-		int tableSize = 13;
+		int tableSize = 319829;
 		int numElements = 0;
 		int bounce = 0;
 		keyValue[] qpArray = new keyValue[tableSize];
@@ -127,25 +113,10 @@ public class Dictionary {
 			return lambda;
 		}
 		
-		// resize
-		public void resize() {
-			keyValue[] doubledQPArray = new keyValue[tableSize*2];
-			for(int i = 0; i < tableSize; i++){
-				doubledQPArray[i] = qpArray[i];
-			}
-			
-			tableSize = tableSize * 2;
-			qpArray = doubledQPArray;
-		}
-		
 		// insert
 		public void insert(String word, String classification, String definition){
 			int key = hash(word);	// retrieve hash key
 			keyValue kvp = new keyValue(key, word, classification, definition); // create the object
-			
-			if(numElements >= (tableSize/2)){
-				resize();
-			}
 			
 			if(qpArray[key] == null){
 				qpArray[key] = kvp;
@@ -174,86 +145,12 @@ public class Dictionary {
 			}
 		}
 	}
-	// ========================
-	//     Separate Chaining
-	// ========================
-	public static class SeparateChaining {
-		int tableSize = 13;
-		int numElements = 0;
-		int bounce = 0;
-		keyValue[] scArray = new keyValue[tableSize];
-		
-		// hash function
-		public int hash(String word) {
-			int hash = 0;
-			for (int i = 0; i < word.length(); i++){
-				hash = (hash << 5) - hash + word.charAt(i);
-			}
-			return Math.abs(hash % tableSize);
-		}
-
-		// return size
-		public int size(){
-			return tableSize;
-		}
-		
-		// resize
-		public void resize() {
-			keyValue[] doubledSCArray = new keyValue[tableSize*2];
-			for(int i = 0; i < tableSize; i++){
-				doubledSCArray[i] = scArray[i];
-			}
-			
-			tableSize = tableSize * 2;
-			scArray = doubledSCArray;
-		}
-		
-		// return lambda
-		public double lambda(){
-			double lambda = ((double)numElements) / tableSize;
-			return lambda;
-		}
-		
-		// insert
-		public void insert(String word, String classification, String definition){
-			int key = hash(word);	// retrieve hash key
-			keyValue kvp = new keyValue(key, word, classification, definition); // create the object
-			
-			if(numElements >= (tableSize/2)){
-				resize();
-			}
-			
-			if(scArray[key] == null){
-				scArray[key] = kvp;
-			} else {
-				while(scArray[key] != null) {
-					key = (key + 1) % tableSize;
-				}
-				scArray[key] = kvp;
-			}
-			numElements++;
-		}
-		
-		// find word
-		public keyValue find(String word) {
-			int key = hash(word);
-			if(scArray[key].key == key){
-				return scArray[key];
-			} else {
-				while(scArray[key].key != key) {
-					key = (key + 1) % tableSize;
-					bounce++;
-				}
-				return scArray[key];
-			}
-		}
-	}
 	
 	// ========================
 	//      DOUBLE HASHING
 	// ========================
 	public static class DoubleHashing {
-		int tableSize = 13;
+		int tableSize = 330767;
 		int numElements = 0;
 		int bounce = 0;
 		keyValue[] dhArray = new keyValue[tableSize];
@@ -286,26 +183,11 @@ public class Dictionary {
 			return lambda;
 		}
 		
-		// resize
-		public void resize() {
-			keyValue[] doubledDHArray = new keyValue[tableSize*2];
-			for(int i = 0; i < tableSize; i++){
-				doubledDHArray[i] = dhArray[i];
-			}
-			
-			tableSize = tableSize * 2;
-			dhArray = doubledDHArray;
-		}
-		
 		// insert
 		public void insert(String word, String classification, String definition){
 			int key = hash1(word);	// retrieve hash key
 			int key2 = hash2(word);
-			
-			if(numElements >= (tableSize/2)){
-				resize();
-			}
-			
+		
 			int doubledhash = (key + key2) % tableSize;
 			
 			keyValue kvp = new keyValue(key, word, classification, definition); // create the object
@@ -340,11 +222,7 @@ public class Dictionary {
 		// Probing
 		LinearProbing lp = new LinearProbing();
 		QuadraticProbing qp = new QuadraticProbing();
-		// Double Hashing
 		DoubleHashing dh = new DoubleHashing();
-		// Chaining
-		SeparateChaining sepc = new SeparateChaining();
-		
 		
 		// Import dictionary
 		try (BufferedReader br = new BufferedReader(new FileReader(FILENAME))){
@@ -355,15 +233,13 @@ public class Dictionary {
 				lp.insert(parts[0], parts[1], parts[2]);
 				qp.insert(parts[0], parts[1], parts[2]);
 				dh.insert(parts[0], parts[1], parts[2]);
-				sepc.insert(parts[0], parts[1], parts[2]);
 			}
+			
 			
 			System.out.println(dh.find(converted_word).word + " " + dh.find(converted_word).classification + " " + dh.find(converted_word).value);
 			System.out.println("linear probing | " + " tablesize: " + lp.tableSize + " " + "lambda: " + lp.lambda() + " " + "bounce: " + lp.bounce);
 			System.out.println("quadratic probing | " + " tablesize: " + qp.tableSize + " " + "lambda: " + qp.lambda() + " " + "bounce: " + qp.bounce);
 			System.out.println("double hashing | " + " tablesize: " + dh.tableSize + " " + "lambda: " + dh.lambda() + " " + "bounce: " + dh.bounce);
-			System.out.println("separate chaining | " + " tablesize: " + sepc.tableSize + " " + "lambda: " + sepc.lambda() + " " + "bounce: " + sepc.bounce);
-			System.out.println(sepc.find(converted_word).value);
 		} catch (IOException e){
 			e.printStackTrace();
 		}
